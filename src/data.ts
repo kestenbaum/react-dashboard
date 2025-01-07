380dc95d02f465b2c8faa4a6d38e62ef0bcfdd0c
Oleksii Vovnenko <kespa71for@gmail.com>
2025-01-07 17:02:30 +0100

    update

--- src/data.ts
+++ src/data.ts
@@ -6,6 +6,7 @@ import {
   ShoppingCart,
   TrendingUp,
   Users,
+  Zap,
 } from 'lucide-react';
 
 export const SIDEBAR_ITEMS = [
@@ -16,4 +17,35 @@ export const SIDEBAR_ITEMS = [
   { name: 'Orders', icon: ShoppingCart, color: '#F59E0B', href: '/orders' },
   { name: 'Analytics', icon: TrendingUp, color: '#3B82F6', href: '/analytics' },
   { name: 'Settings', icon: Settings, color: '#6EE7B7', href: '/settings' },
+];
+
+export const dataCards = [
+  {
+    id: 1,
+    name: 'Total Sales',
+    icon: Zap,
+    value: '12.345',
+    color: '#6366F1',
+  },
+  {
+    id: 2,
+    name: 'New Users',
+    icon: Users,
+    value: '1.234',
+    color: '#8B5CF6',
+  },
+  {
+    id: 3,
+    name: 'Total Products',
+    icon: ShoppingBag,
+    value: '567',
+    color: '#EC4899',
+  },
+  {
+    id: 4,
+    name: 'Conversion Rate',
+    icon: BarChart2,
+    value: '12.5%',
+    color: '#10B981',
+  },
 ];
--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,5 +1,39 @@
+import { motion } from 'framer-motion';
+import Header from '../components/common/Header';
+import StatCard from '../components/common/StatCard';
+import CategoryDistributionChart from '../components/overview/CategoryDistributionChart';
+import SalesOverviewChart from '../components/overview/SalesOverviewChart';
+import { dataCards } from '../data';
+
 const OverviewPage = () => {
-  return <div>OverviewPage</div>;
+  return (
+    <section className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8 "
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 20 }}
+          transition={{ duration: 1 }}
+        >
+          {Array.isArray(dataCards) &&
+            dataCards.map((element) => (
+              <StatCard
+                key={element.id}
+                name={element.name}
+                value={element.value}
+                icon={element.icon}
+                color={element.color}
+              />
+            ))}
+        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <SalesOverviewChart />
+          <CategoryDistributionChart />
+        </div>
+      </main>
+    </section>
+  );
 };
 
 export default OverviewPage;
